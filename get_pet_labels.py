14f412384c78f17294e0854550f62be887d78c78
Jacob Lapenna <jlapenna@usbr.gov>
2022-06-01 16:41:47 +0000

    Updated comments and dictionary values to lists as requirement dictates.

--- get_pet_labels.py
+++ get_pet_labels.py
@@ -16,7 +16,8 @@
 #          at index 0 : pet image label (string).
 #
 ##
-# Imports python modules
+
+# import python modules
 from os import listdir
 
 # TODO 2: Define get_pet_labels function below please be certain to replace None
@@ -28,16 +29,14 @@ def get_pet_labels(image_dir):
     Creates a dictionary of pet labels (results_dic) based upon the filenames 
     of the image files. These pet image labels are used to check the accuracy 
     of the labels that are returned by the classifier function, since the 
-    filenames of the images contain the true identity of the pet in the image.
-    Be sure to format the pet labels so that they are in all lower case letters
-    and with leading and trailing whitespace characters stripped from them.
+    filenames of the images contain the true identity of the animal in the image.
     (ex. filename = 'Boston_terrier_02259.jpg' Pet label = 'boston terrier')
     Parameters:
      image_dir - The (full) path to the folder of images that are to be
                  classified by the classifier function (string)
     Returns:
       results_dic - Dictionary with 'key' as image filename and 'value' as a 
-      List. The list contains for following item:
+      List. The list contains the following item:
          index 0 = pet image label (string)
     """
     results_dic = {}
@@ -52,7 +51,7 @@ def get_pet_labels(image_dir):
                 words.append(word.lower())
         # join all words to form value
         value = " ".join(words).strip()
-        results_dic[key] = value
+        results_dic[key] = [value]
     
     # return the dictionary containing {file : label} pairs
     return results_dic
